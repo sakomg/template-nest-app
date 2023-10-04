72a5697242ba3b7814df98b7bb7fb6059b7aca8c
Alexander Komegunov <a_komegunov@twistellar.com>
2023-10-04 22:30:54 +0300

    add getObjectDescribe and insertRecord to service

--- src/salesforce/salesforce.service.ts
+++ src/salesforce/salesforce.service.ts
@@ -10,11 +10,23 @@ export class SalesforceService {
   }
 
   async getRecords(sObjectName: string, fields: Array<string>, additionalClause: string) {
-    const records = await this.api.fetchAllRecords(`SELECT ${fields.join(', ')} FROM ${sObjectName} ${additionalClause}`);
+    const records = await this.api.fetchAllRecords(`
+      SELECT ${fields.join(', ')} 
+      FROM ${sObjectName} 
+      ${additionalClause}
+    `);
     return records;
+  }
+
+  async getObjectDescribe(sObjectName: string) {
+    return await this.api.fetchObjectDescribe(sObjectName);
   }
 
   async upsertRecords(sObjectName: string, records: Array<any>, externalId: any) {
     await this.api.upsertRecords(records, sObjectName, externalId);
+  }
+
+  async insertRecord(sObjectName: string, record: object) {
+    await this.api.insertRecord(sObjectName, record);
   }
 }
--- src/schedulers/service.ts
+++ src/schedulers/service.ts
@@ -1,20 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { SalesforceService } from 'src/salesforce/salesforce.service';
 import { SftpService } from 'src/sftp/sftp.service';
 
 @Injectable()
 export class Service {
-  constructor(
-    private readonly sftp: SftpService,
-    private readonly salesforce: SalesforceService,
-    private readonly config: ConfigService,
-  ) {}
+  constructor(private readonly sftp: SftpService, private readonly salesforce: SalesforceService) {}
 
   async execute() {
     await Promise.all([this.salesforce.doConnect(), this.sftp.doConnect()]);
     const accounts = await this.salesforce.getRecords('Account', ['Id', 'Name'], 'LIMIT 100');
-    const files = await this.sftp.listFiles(this.config.get('SFTP_PATH'), '.csv');
+    const files = await this.sftp.listFiles('/FFUN/Dealervu/', '.csv');
     console.log(accounts);
     console.log(files);
   }
